41fbe83c5cd6bc2fec46a5dd8959d81f2f571f96
PRIYA MANNA <priyamanna0111@gmail.com>
2025-05-13 13:33:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const githubLinks = {
+  "1990": "https://pri0111.github.io/BANGALORE_1990/",
+  "2000": "https://pri0111.github.io/BANGALORE_2000/",
+  "2010": "https://pri0111.github.io/BANGALORE_2010/",
+  "2020": "https://pri0111.github.io/BANGALORE_2020/"
+};
+
+const services = [
+    "Provisional Service of Agricultural Land","Regulating Service of Agricultural Land","Supporting Service of Agricultural Land","Cultural Service of Agricultural Land",
+    "Provisional Service of Shrub", "Regulating Service of Shrub", "Supporting Service of Shrub","Cultural Service of Shrub",
+    "Provisional Service of Waterbody", "Regulating Service of Waterbody","Supporting Service of Waterbody", "Cultural Service of Waterbody",
+    "Provisional Service of Vegetation","Regulating Service of Vegetation", "Supporting Service of Vegetation", "Cultural Service of Vegetation"
+  ];
+ 
